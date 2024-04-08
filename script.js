a24825ccd653d1ff995b93f6e743bc2e91afe171
Duy Nguyen <s3878076@rmit.edu.vn>
2024-04-08 13:55:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /// ==UserScript==
-// @name        Path Of Exile Trade Aggregator pathofexile.com/trade
+// @name        Path Of Exile Trade Aggregator
 // @namespace   Violentmonkey Scripts
 // @match       https://www.pathofexile.com/trade*
 // @grant       none
